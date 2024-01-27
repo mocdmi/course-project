184b4287f13e7109f4b5c941e4d10d7a2ff8b32b
Dmitriy Mosin <i@dmosin.ru>
2024-01-27 18:03:33 +0300

    fix CI/CD

--- src/entities/Profile/model/slice/profileSlice.test.ts
+++ src/entities/Profile/model/slice/profileSlice.test.ts
@@ -2,7 +2,7 @@ import { Currency } from 'entities/Currency';
 import { Country } from 'entities/Country';
 import { updateProfileData } from '../services/updateProfileData/updateProfileData';
 import { profileActions, profileReducer } from './profileSlice';
-import { ProfileSchema, ValidateProfileError } from '../types/Profile';
+import { ProfileSchema, ValidateProfileError } from '../types/profile';
 
 const data = {
     first: 'Дмитрий',
